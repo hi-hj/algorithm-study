f8e3a9bcf0276cb39f226f7f17c0af65bf3638f7
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2021-04-29 23:47:53 +0900

    2021.04.28

--- LeetCode/739_daily_teperatures.py
+++ LeetCode/739_daily_teperatures.py
@@ -1,3 +1,19 @@
 class Solution(object):
     def dailyTemperatures(self, T):
-        
+        result = [0]*len(T)
+        stack = []
+        for i, cur in enumerate(T):
+            print(i, stack)
+            while stack and cur > T[stack[-1]]:
+                last = stack.pop()
+                print(last)
+                result[last] = i-last
+            stack.append(i)
+        print(result)
+        return result
+            
+
+
+
+T = [73, 74, 75, 71, 69, 72, 76, 73]
+Solution().dailyTemperatures(T)
