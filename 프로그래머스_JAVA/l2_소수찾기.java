5b2a10eebab1be24f9a8e93ab0bea4953185cbc1
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2021-07-16 00:18:11 +0900

    2021.07.15

--- 프로그래머스_JAVA/l2_소수찾기.java
+++ 프로그래머스_JAVA/l2_소수찾기.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class l2_소수찾기 {
     public static void main(String[] args){
         String numbers = "17";
@@ -6,10 +8,34 @@ public class l2_소수찾기 {
     }
 
     public static int solution(String numbers){
-        int[] canMakeList;
+        HashSet<Integer> set = new HashSet<>();
+        permutation("", numbers, set);
+        int count= 0;
+        while(set.iterator().hasNext()){
+            int a = set.iterator().next();
+            set.remove(a);
+            if(a==2) count++;
+            if(a%2!=0 && isPrime(a)) count++;
 
+        }
 
-        int answer = 0;
-        return answer;
+        return count;
+    }
+
+    public static boolean isPrime(int n ){
+        if(n==0 || n==1) return false;
+        for(int i=3; i<=(int)Math.sqrt(n); i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+
+    public static void permutation(String prefix, String str, HashSet<Integer> set){
+        int n = str.length();
+
+        if(!prefix.equals("")) set.add(Integer.valueOf(prefix));
+        for (int i =0; i<n; i++){
+            permutation(prefix + str.charAt(i), str.substring(0,i)+str.substring(i+1,n), set);
+        }
     }
 }
