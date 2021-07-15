94cf4d7c3ebd308c71a4428f5c7b173614cf92b3
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2021-07-15 23:49:30 +0900

    2021.07.15

---
+++ 프로그래머스_JAVA/l2_소수찾기.java
@@ -0,0 +1,15 @@
+public class l2_소수찾기 {
+    public static void main(String[] args){
+        String numbers = "17";
+        int answer = solution(numbers);
+
+    }
+
+    public static int solution(String numbers){
+        int[] canMakeList;
+
+
+        int answer = 0;
+        return answer;
+    }
+}
